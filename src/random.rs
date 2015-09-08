199c9561b8bb58f6a36845aec9f7f8a0c7f5cdec
Michael Eden <themichaeleden@gmail.com>
2015-09-08 13:21:18 -0400

    More improvements and restructuring

--- src/random.rs
+++ src/random.rs
@@ -6,29 +6,27 @@ const UNMISTAKABLE_CHARS: &'static str = "23456789ABCDEFGHJKLMNPQRSTWXYZabcdefgh
 
 pub struct Random {
     rng: rand::OsRng,
+    set: Vec<char>,
 }
 
 impl Random {
 	pub fn new() -> Self {
 		Random {
 			rng: rand::OsRng::new().unwrap(),
+            set: UNMISTAKABLE_CHARS.chars().collect(),
 		}
 	}
 
 	pub fn id(&mut self) -> String {
-		self.random_string(17, UNMISTAKABLE_CHARS)
+		self.random_string(17)
 	}
 
-	pub fn random_string(&mut self, char_count: usize, alphabet: &'static str) -> String {
+	pub fn random_string(&mut self, char_count: usize) -> String {
 		let mut rand_str = String::with_capacity(char_count);
 		for _ in 0..char_count {
-			rand_str.push(self.pick_char(alphabet));
+            let next = (self.rng.next_f32() * self.set.len() as f32) as usize;
+			rand_str.push(self.set.get(next).unwrap().clone());
 		}
 		rand_str
-	}
-
-	pub fn pick_char(&mut self, alphabet: &'static str) -> char {
-		// TODO: Make faster
-		alphabet.chars().nth((self.rng.next_f32() * alphabet.len() as f32) as usize).unwrap()
 	}
 }
